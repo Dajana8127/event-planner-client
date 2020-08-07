63e62355e95ae1ab5a0c28fbadc1e8b9b0503d49
Dajana8127 <dajana.stankovic94@gmail.com>
2020-08-07 16:08:32 -0400

    Functional version 1 show, update and delete event

--- src/components/Events/EventShow.js
+++ src/components/Events/EventShow.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import { Redirect } from 'react-router-dom'
 
 import apiUrl from '../../apiConfig'
 
@@ -8,7 +11,9 @@ class EventIndex extends Component {
     super()
 
     this.state = {
-      event1: ''
+      event1: '',
+      updated: false,
+      deleted: false
     }
   }
   componentDidMount () {
@@ -17,7 +22,7 @@ class EventIndex extends Component {
     const id = this.props.match.params.id
     axios({
       method: 'GET',
-      url: apiUrl + `/events/${id}`,
+      url: apiUrl + `/events/${id}/`,
       headers: {
         'Authorization': `Token ${user.token}`
       }
@@ -27,11 +32,83 @@ class EventIndex extends Component {
         this.setState({
           event1: response.data
         })
-        this.setState(response.data)
       })
       .catch(console.error)
   }
+
+  handleChange = event => {
+    event.preventDefault()
+    console.log('clicked', event)
+    // get the key from the input name field
+    const event1Key = event.target.name
+    // get the input value that the user typed in
+    const value = event.target.value
+    // make a copy of the current state
+    const event1Copy = Object.assign({}, this.state.event1)
+    // update the copy with the new user input
+    event1Copy[event1Key] = value
+    // update the state with the updated copy
+    this.setState({ event1: event1Copy })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const id = this.props.match.params.id
+    console.log(id)
+    const { msgAlert, user } = this.props
+    axios({
+      method: 'PATCH',
+      url: `${apiUrl}/events/${id}/`,
+      headers: {
+        'Authorization': `Token ${user.token}`
+      },
+      data: {
+        event1: this.state.event1
+      }
+    })
+      .then(response => {
+        this.setState({
+          updated: true,
+          event1: response.data
+        })
+      })
+      .then(() => msgAlert({
+        heading: 'Event Update Success',
+        message: 'You updated event successfully',
+        variant: 'success'
+      }))
+      .catch(console.error)
+  }
+
+  deleteEvent = event => {
+    const id = this.props.match.params.id
+    const { msgAlert, user } = this.props
+    console.log('clicked', event)
+    axios({
+      method: 'DELETE',
+      url: `${apiUrl}/events/${id}`,
+      headers: {
+        'Authorization': `Token ${user.token}`
+      }
+    })
+      .then(() => {
+        this.setState({
+          deleted: true
+        })
+      })
+      .then(() => msgAlert({
+        heading: 'Event Deleted Successfully',
+        message: 'The event is deleted',
+        variant: 'success'
+      }))
+      .catch(console.error)
+  }
+
   render () {
+    // const id = this.props.match.params.id
+    if (this.state.updated === true || this.state.deleted === true) {
+      return <Redirect to={'/my-events'}></Redirect>
+    }
     let jsx
     // if the API has not responded yet
     if (this.state.event1 === null) {
@@ -41,11 +118,58 @@ class EventIndex extends Component {
       jsx = (
         <div className="row event-show">
           <div className="col-sm-10 col-md-8 mx-auto mt-5">
-            <section>
-              <h3>{this.state.event1.name}</h3>
-              <h4>{this.state.event1.place}</h4>
-              <h4>{this.state.event1.description}</h4>
-            </section>
+            <h1>Update Event</h1>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group controlId="name">
+                <Form.Label>Event Name</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  name="name"
+                  value={this.state.event1.name}
+                  placeholder="Enter event name"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="place">
+                <Form.Label>Location</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  name="place"
+                  value={this.state.event1.place}
+                  placeholder="Location"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="description">
+                <Form.Label>Event Description</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  name="description"
+                  value={this.state.event1.description}
+                  placeholder="What are you doing?"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Button
+                className="btn"
+                variant="primary"
+                type="submit"
+              >
+                Update
+              </Button>
+            </Form>
+            <Form onSubmit={this.deleteEvent}>
+              <Button
+                className="btn"
+                variant="primary"
+                type="submit"
+              >
+                Delete
+              </Button>
+            </Form>
           </div>
         </div>
       )
@@ -60,3 +184,7 @@ class EventIndex extends Component {
 }
 
 export default EventIndex
+
+// <h3>{this.state.event1.name}</h3>
+// <h4>{this.state.event1.place}</h4>
+// <h4>{this.state.event1.description}</h4>
